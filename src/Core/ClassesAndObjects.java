4b5221e41c00ba60f088f37a94fcbde689380333
yury.uvarov <Yury.Uvarov@dataart.com>
2020-07-05 12:16:46 +0300

    updated ClassesAndObjects

--- src/Core/ClassesAndObjects.java
+++ src/Core/ClassesAndObjects.java
@@ -2,12 +2,39 @@ package Core;
 
 public class ClassesAndObjects {
 
-    class test1{
+    final int AGE_TO_PENS = 65;
 
-        class test2 {
+    public static void main(String[] args) {
 
+        Person person1 = new Person();
+        person1.setName("Age", 10);
+        String s1 = "Lusja";
+        int a1 = 10000;
+        person1.setName(s1, a1);
+        String ps = person1.speakPerson();
+
+        Person person2 = new Person();
+        person2.setName("ANKA", 20);
+        String ps2 = person2.speakPerson();
+
+    }
+}
+
+
+class Person {
+        String name = "defoult name";
+        int age = 23;
+
+
+        void setName(String x, int a){
+         this.name = x;
+         this.age = a;
+        }
+
+
+String speakPerson(){
+
+             System.out.println(name + " " + age);
+             return name + age;
         }
     }
-
-
-}
